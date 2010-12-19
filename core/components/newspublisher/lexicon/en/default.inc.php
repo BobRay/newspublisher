361e183486f150361aa8706a120e503baecef1be
bobray <bobray@softville.com>
2010-12-19 15:32:30 -0500

    Added missing Lexicon strings

--- core/components/newspublisher/lexicon/en/default.inc.php
+++ core/components/newspublisher/lexicon/en/default.inc.php
@@ -53,3 +53,70 @@ $_lang['np_save_permission_denied'] = 'You do not have permission to view this d
 $_lang['np_no_edit_self'] = 'You cannot edit the newspublisher page.';
 $_lang['np_no_parent_groups'] = '&groups set to parent but doc has no parent';
 $_lang['np_post_save_no_resource'] = 'Unable to get resource after save';
+
+/* missing resource field lexicon strings */
+$_lang['resource_pub_date'] =  'Publish Date';
+$_lang['resource_pub_date_help'] =  ' (optional) set date to automatically publish resource. Click on the calendar icon to select a date.';
+$_lang['resource_unpub_date'] =  'Unpublish Date';
+$_lang['resource_unpub_date_help'] =  ' (optional) set date to automatically unpublish the resource. Click on the calendar icon to select a date.';
+$_lang['resource_hidemenu'] =  'Hide from Menus';
+$_lang['resource_hidemenu_help'] =  'When enabled the resource will'. '<b>' . 'not' . '</b>' . 'be available for use inside a web menu. Please note that some Menu Builders might choose to ignore this option.';
+$_lang['resource_isfolder'] =  'Container';
+$_lang['resource_isfolder_help'] =  'Check this to designate the Resource as a Container for other Resources. A `Container` is like a folder, but it can also have content of its own.';
+$_lang['resource_content_dispo'] =  'Content Disposition';
+$_lang['resource_content_dispo_help'] =  'Use the content disposition field to specify how this resource will be handled by the web browser. For file downloads select the Attachment option.';
+$_lang['resource_class_key'] =  'Class Key';
+$_lang['resource_context_key'] =  'Context Key';
+$_lang['resource_context_key_help'] =  'The context the resource is attached to.';
+$_lang['resource_publishedby_help'] =  'The ID of the user who published the resource.';
+$_lang['resource_createdby_help'] =  'The ID of the user who originally created the resource.';
+$_lang['resource_createdon_help'] =  'The date the resource was originally created.';
+$_lang['resource_editedby_help'] =  'The ID of the user who most recently edited the resource.';
+$_lang['resource_editedon_help'] =  'The date the resource was most recently edited.';
+$_lang['resource_deleted'] =  'Deleted';
+$_lang['resource_deleted_help'] =  'Marks the resource for deletion, but does not delete it.';
+$_lang['resource_deletedon'] =  'Deleted On';
+$_lang['resource_deletedon_help'] =  'The date the resource was most recently marked for deletion.';
+$_lang['resource_deletedby'] =  'Deleted By';
+$_lang['resource_deletedon_help'] =  'The ID of the user who most recently marked the resource for deletion.';
+$_lang['resource_type'] =  'Resource Type';
+$_lang['resource_type_help'] =  'The type of the resource (e.g., document, weblink, symlink, or static resource';
+$_lang['resource_contentType'] =  'Content Type';
+$_lang['resource_contentType_help'] =  "The resource's Content type (e.g., text/html).";
+$_lang['resource_id'] =  'Resource ID';
+$_lang['resource_id_help'] =  'The resource ID of the resource.';
+$_lang['resource_content_help'] =  'The main content field of the resource.';
+$_lang['resource_introtext'] =  'Summary (introtext)';
+$_lang['resource_introtext_help'] =  "A brief summary of the resource's content.";
+$_lang['resource_donthit'] =  "Don't Hit";
+$_lang['resource_donthit_help'] =  'Deprecated.';
+$_lang['resource_haskeywords'] =  'Has Key Words';
+$_lang['resource_haskeywords_help'] =  'Deprecated.';
+$_lang['resource_hasmetatags'] =  'Has Meta Tags';
+$_lang['resource_hasmetatags_help'] =  'Deprecated.';
+$_lang['resource_privateweb'] =  'Private Web';
+$_lang['resource_privateweb_help'] =  'Deprecated.';
+$_lang['resource_privatemgr'] =  'Private Manager';
+$_lang['resource_privatemgr_help'] =  'Deprecated.';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
