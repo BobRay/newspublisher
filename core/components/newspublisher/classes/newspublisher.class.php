243a0a2081aa59760612af12224fa4474c2332bc
bobray <bobray@softville.com>
2011-01-04 21:59:09 -0500

    fixed HTML bug in image tpl

--- core/components/newspublisher/classes/newspublisher.class.php
+++ core/components/newspublisher/classes/newspublisher.class.php
@@ -477,7 +477,7 @@ public function getTpls() {
       /* These Tpls are used for TVs of various types */
     $this->tpls['imageTpl'] = ! empty ($this->props['imagetpl'])? $this->modx->getChunk($this->props['imagetpl']) : "\n" . '[[+[[+prefix]].error_[[+npx.fieldName]]]]
     <label for="[[+npx.fieldName]]" title="[[+npx.help]]">[[+npx.caption]]: </label>
-        <input name="[[+npx.fieldName]]" class="image" id="[[+npx.fieldName]]" type="text" value="[[+[[+prefix]].[[+npx.fieldName]]]]" /><img class="np-image" alt="[[+npx.fieldName]]" src="[[+[[+prefix]].[[+npx.fieldName]]]]">';
+        <input name="[[+npx.fieldName]]" class="image" id="[[+npx.fieldName]]" type="text" value="[[+[[+prefix]].[[+npx.fieldName]]]]" /><img class="np-image" alt="[[+npx.fieldName]]" src="[[+[[+prefix]].[[+npx.fieldName]]]]" />';
 
 
     /* These are for the outer shell of listboxes, checkboxes, and radio options */
