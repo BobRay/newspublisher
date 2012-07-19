7f49538b43562c2167053e64263a6525fc37d754
bobray <bobray@softville.com>
2012-07-19 03:15:47 -0500

    bumped version to 1.4.1-rc

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -61,7 +61,7 @@ $modx->setLogTarget(XPDO_CLI_MODE ? 'ECHO' : 'HTML');
 
 /* set package info */
 define('PKG_NAME','newspublisher');
-define('PKG_VERSION','1.4.0');
+define('PKG_VERSION','1.4.1');
 define('PKG_RELEASE','rc');
 
 /* load builder */
