3460701277715a5d2f3f9d796ceaa4b21f4a6b83
bobray <bobray@softville.com>
2016-12-17 13:58:57 -0600

    Fix encoding error in datepicker

--- assets/components/newspublisher/datepicker/js/datepicker.js
+++ assets/components/newspublisher/datepicker/js/datepicker.js
@@ -7,13 +7,13 @@
         
         You are free:
         
-        * to Share — to copy, distribute and transmit the work
-        * to Remix — to adapt the work
+        * to Share - to copy, distribute and transmit the work
+        * to Remix - to adapt the work
             
         Under the following conditions:
 
-        * Attribution — You must attribute the work in the manner specified by the author or licensor (but not in any way that suggests that they endorse you or your use of the work).      
-        * Share Alike — If you alter, transform, or build upon this work, you may distribute the resulting work only under the same, similar or a compatible license.
+        * Attribution - You must attribute the work in the manner specified by the author or licensor (but not in any way that suggests that they endorse you or your use of the work).
+        * Share Alike - If you alter, transform, or build upon this work, you may distribute the resulting work only under the same, similar or a compatible license.
 */
 
 var datePickerController = (function datePickerController() {
@@ -243,7 +243,7 @@ var datePickerController = (function datePickerController() {
                         } else if(/lang|buttontabindex|mousewheel|cellformat|titleformat|nodrag|describedby/.test(str.toLowerCase())) {                                               
                                 var f = Function(['var document,top,self,window,parent,Number,Date,Object,Function,',
                                         'Array,String,Math,RegExp,Image,ActiveXObject;',
-                                        'return (' , str.replace(/<\!--.+-->/gim,'').replace(/\bfunction\b/g,'function­') , ');'].join(''));
+                                        'return (' , str.replace(/<\!--.+-->/gim,'').replace(/\bfunction\b/g,'functionï¿½') , ');'].join(''));
                                 return f();                          
                         };
                 } catch (e) { };
