003a241043eb1fbe870c600a05a5af0d7c0c5e44
bobray <bobray@softville.com>
2017-02-20 23:07:43 -0600

    Correct typo in lex file

--- core/components/newspublisher/lexicon/en/setting.inc.php
+++ core/components/newspublisher/lexicon/en/setting.inc.php
@@ -1,7 +1,7 @@
 <?php
 /* System Settings */
 
-$_lang['setting_np_elfinder_theme_desc'] = 'Theme for elFinder to use in NewsPublisher; build-in themes include: osx, windows-10, moono, material; Default: osx.';
+$_lang['setting_np_elfinder_theme_desc'] = 'Theme for elFinder to use in NewsPublisher; built-in themes include: osx, windows-10, moono, material; Default: osx.';
 $_lang['setting_np_elfinder_remember_last_dir_desc'] = 'Make elFinder remember last directory browsed (must be the string true or false); default: false.';
 $_lang['setting_np_elfinder_tmb_size_desc'] = 'Thumb size for elFinder in NewsPublisher (in pixels); default: 150';
 $_lang['setting_np_tinymce_skin_desc'] = 'Skin directory for TinyMCE skin in assets/components/newspublisher/tinymceskins; built-in options: modxPericles, fairOphelia, fallenMacbeth, light; default: modxPericles';
