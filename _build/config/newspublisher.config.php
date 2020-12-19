c8cda5a2d82be5979904bbfefbf965f74e9492b1
bobray <bobray@softville.com>
2020-12-18 19:14:39 -0500

    Version to 3.1.0-pl -- released

--- _build/config/newspublisher.config.php
+++ _build/config/newspublisher.config.php
@@ -16,14 +16,14 @@ $components = array(
     'packageName' => 'NewsPublisher',  /* No spaces, no dashes */
     'packageNameLower' => $packageNameLower,
     'packageDescription' => 'NewsPublisher project for MyComponent extra',
-    'version' => '3.0.4',
+    'version' => '3.1.0',
     'release' => 'pl',
     'author' => 'Bob Ray',
     'email' => '<https://bobsguides.com>',
     'authorUrl' => 'https://bobsguides.com',
     'authorSiteName' => "Bob's Guides",
     'packageDocumentationUrl' => 'https://bobsguides.com/newspublisher-tutorial.html',
-    'copyright' => '2013-2017',
+    'copyright' => '2013-2021',
 
     /* no need to edit this except to change format */
     'createdon' => strftime('%m-%d-%Y'),
